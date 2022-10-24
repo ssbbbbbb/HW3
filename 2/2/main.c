be462fd53ec9e2be5973102fec0775e792a8dc6c
ssbbbbbb <ben30794013@gmail.com>
2022-10-24 14:28:19 +0800

    HW3

---
+++ 2/2/main.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+double Power(double, int);
+
+void main()
+{
+	int k;
+	double ans;
+	printf("計算3.5的k次方?請輸入k=");
+	scanf("%d", &k);
+	ans = Power(3.5, k);
+	printf("3.5的%d次方=%f\n", k, ans);
+	system("pause");
+}
+
+double Power(double X, int n)
+{
+	int i;
+	double PowerXn = 1;
+	for (i = 1;i <= n;i++)
+		PowerXn = PowerXn * X;
+	return PowerXn;
+}
---
+++ 3/3/main.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int maximum(int, int, int);
+
+int main()
+{
+	int number1, number2, number3;
+	printf("Enter three integers :");
+	scanf("%d %d %d", &number1, &number2, &number3);
+	printf("Maximum is : %d\n", maximum(number1, number2, number3));
+	system("pause");
+	return 0;
+}
+
+int maximum(int x, int y, int z)
+{
+	int max = x;
+	if (y > max)
+		max = y;
+	if (z > max)
+		max = z;
+
+	return max;
+}
